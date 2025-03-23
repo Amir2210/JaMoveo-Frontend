a8ce1fdf1cdf7da2c81a17a2798c7ed48ee87e39
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-23 16:54:16 +0200

    Create AdminSearchSong page

--- src/pages/AdminSearchSong.jsx
+++ src/pages/AdminSearchSong.jsx
@@ -1,5 +1,35 @@
+import { Link } from 'react-router';
+
 export function AdminSearchSong() {
   return (
-    <h1>admin looking for song</h1>
+    <section className='main-bg h-screen'>
+      <div className="small-container sm:big-container">
+        <div className='navbar py-6'>
+          <div className='text-4xl secondary-bg flex text-white font-mono font-bold size-14 justify-center items-center rounded-lg'>J</div>
+          <div className=' ml-4 text-3xl font-bold tracking-wide text-white'>jaMoveo</div>
+        </div>
+        <div className='grid sm:grid-cols-2 gap-10 mt-5 sm:mt-20 text-[#B3B3B3]'>
+          <div>
+            <h1 className='text-5xl sm:text-6xl font-bold tracking-wide capitalize mb-6'>Search any <span className='secondary-txt'>song...</span> </h1>
+            <label className="input input-bordered flex items-center gap-2">
+              <input type="text" className="grow" placeholder="Search" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd" />
+              </svg>
+            </label>
+          </div>
+          <div>
+            <img className='size-48 sm:size-full' src="https://res.cloudinary.com/dxm0sqcfp/image/upload/v1742741494/jamoveo/undraw_happy-music_na4p_ir0kkh.svg" alt="Job Tracking App" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
